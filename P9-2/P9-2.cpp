e93461f59ccda36126582913b0e1b1c0d6921d75
Atherton Bunner <a-bunner@onu.edu>
2026-04-02 09:19:57 -0400

    Completed isSortedDecreasing

--- P9-2/P9-2.cpp
+++ P9-2/P9-2.cpp
@@ -3,9 +3,9 @@ using namespace std;
 // atherton Bunner; functionakl arrays
 
 bool isSortedIncreasing(int values[], int size);
-bool isSortedDecreasing(int, int);
-bool hasAdjecentDuplicates(int, int);
-bool hasDuplicates(int, int);
+bool isSortedDecreasing(int values[], int size);
+bool hasAdjecentDuplicates(int values[], int size);
+bool hasDuplicates(int values[], int size);
 int main() {
 	const int SIZE = 6;
 	int values[SIZE];
@@ -21,12 +21,29 @@ int main() {
 	else {
 		cout << "The data are not increasing" << endl;
 	}
+
+	if (isSortedDecreasing(values, SIZE)) { // checks if decreasing
+		cout << "The data are decreasing" << endl;
+	}
+	else {
+		cout << "The data are not decreasing" << endl;
+	}
 }
 
 // fuction no1
 bool isSortedIncreasing(int values[], int size) {
 	for (int j = 0; j < size - 1; j++) { // checking every values
 		if (values[j] > values[j + 1]) {
+			return false;
+		}
+	} // if never triggered, then
+	return true;
+}
+
+// fuction no2
+bool isSortedDecreasing(int values[], int size) {
+	for (int k = 0; k < size - 1; k++) { // checking values
+		if (values[k] < values[k + 1]) { // opposite of increasing
 			return false;
 		}
 	} // if never triggered, then
